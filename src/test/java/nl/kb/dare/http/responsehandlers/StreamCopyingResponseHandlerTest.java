30c3154399239a521f0cb60e30f9c4659d76f9a7
Rene van der Ark <r.van.der.ark@gmail.com>
2017-02-14 09:28:27 +0100

    adds itShouldLogIOExceptions to StreamCopyingResponseHandlerTest.

--- src/test/java/nl/kb/dare/http/responsehandlers/StreamCopyingResponseHandlerTest.java
+++ src/test/java/nl/kb/dare/http/responsehandlers/StreamCopyingResponseHandlerTest.java
@@ -5,10 +5,16 @@ import org.junit.Test;
 
 import javax.ws.rs.core.Response;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.mock;
 
 public class StreamCopyingResponseHandlerTest {
 
@@ -32,5 +38,18 @@ public class StreamCopyingResponseHandlerTest {
         instance.onResponseData(Response.Status.OK, StreamCopyingResponseHandler.class.getResourceAsStream("/http/text.txt"));
 
         assertThat(new String(checksumOut.toByteArray(), Charset.forName("UTF8")), is("ae2b1fca515949e5d54fb22b8ed95575"));
+    }
+
+    @Test
+    public void itShouldLogIOExceptions() throws IOException {
+        final OutputStream out = mock(OutputStream.class);
+        final ByteArrayOutputStream checksumOut = new ByteArrayOutputStream();
+        final HttpResponseHandler instance = new ResponseHandlerFactory().getStreamCopyingResponseHandler(out, checksumOut);
+
+        doThrow(IOException.class).when(out).write(any(), anyInt(), anyInt());
+
+        instance.onResponseData(Response.Status.OK, StreamCopyingResponseHandler.class.getResourceAsStream("/http/text.txt"));
+
+        assertThat(instance.getExceptions().size(), is(1));
     }
 }
