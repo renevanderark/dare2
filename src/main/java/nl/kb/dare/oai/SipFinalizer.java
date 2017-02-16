66ec72b8d0428ecbf1e4bf7899244f4fe85c5366
Rene van der Ark <r.van.der.ark@gmail.com>
2017-02-16 07:47:34 +0100

    extracts sip logic to methods.

--- src/main/java/nl/kb/dare/oai/SipFinalizer.java
+++ src/main/java/nl/kb/dare/oai/SipFinalizer.java
@@ -50,44 +50,74 @@ class SipFinalizer {
 
             for (int i = 0; i < fileNodes.getLength(); i++) {
                 final Node fileNode = fileNodes.item(i);
-                final NamedNodeMap fileAttributes = fileNode.getAttributes();
-                final Node checksum = document.createAttribute("CHECKSUM");
-                final Node checksumType = document.createAttribute("CHECKSUMTYPE");
-                final String fileId = fileAttributes.getNamedItem("ID").getNodeValue();
-                final Node fLocatNode = getFLocatNode(fileNode);
 
-
-                final Optional<ObjectResource> currentResource = objectResources
-                        .stream().filter(obj -> obj.getId() != null && obj.getId().equals(fileId))
-                        .findAny();
-
-                if (!currentResource.isPresent()) {
-                    throw new IOException("Expected file resource is not present for metadata.xml: " + fileId);
+                final Optional<String> fileId = getAttribute(fileNode, "ID");
+                if (!fileId.isPresent()) {
+                    throw new IOException("ID attribute not set for file node in metadata.xml");
                 }
 
-                checksum.setNodeValue(currentResource.get().getChecksum());
-                checksumType.setNodeValue(currentResource.get().getChecksumType());
-                fileAttributes.setNamedItem(checksum);
-                fileAttributes.setNamedItem(checksumType);
-                fLocatNode.getAttributes().getNamedItemNS(XLINK_NS, "href").setNodeValue(
-                        "file://./resources/" +
-                                URLEncoder.encode(currentResource.get().getLocalFilename(), "UTF8")
-                                        .replaceAll("\\+", "%20")
-                );
+                final Optional<ObjectResource> currentResource = findObjectResourceForFileId(objectResources, fileId.get());
+                if (!currentResource.isPresent()) {
+                    throw new IOException("Expected file resource is not present for metadata.xml: " + fileId.get());
+                }
+
+                setAttribute(document, fileNode, "CHECKSUM", currentResource.get().getChecksum());
+                setAttribute(document, fileNode, "CHECKSUMTYPE", currentResource.get().getChecksumType());
+                setXlinkHref(fileNode, currentResource.get());
             }
             transformer.transform(new DOMSource(document), new StreamResult(sip));
         }
     }
 
-    private Node getFLocatNode(Node fileNode) {
-        final NodeList childNodes = fileNode.getChildNodes();
+    private void setXlinkHref(Node fileNode, ObjectResource currentResource) throws IOException {
+        final Optional<Node> fLocatNode = getFirstChildByLocalName(fileNode, "FLocat");
+        if (!fLocatNode.isPresent()) {
+            throw new IOException("File node does not have an FLocat child in metadata.xml");
+        }
+
+        final Node xlinkHrefAttribute = fLocatNode.get().getAttributes().getNamedItemNS(XLINK_NS, "href");
+        if (xlinkHrefAttribute == null) {
+            throw new IOException("FLocat node does not have xlink:href attribute in metadata.xml");
+        }
+
+        xlinkHrefAttribute.setNodeValue(
+                "file://./resources/" +
+                        URLEncoder.encode(currentResource.getLocalFilename(), "UTF8")
+                                .replaceAll("\\+", "%20")
+        );
+    }
+
+    private Optional<ObjectResource> findObjectResourceForFileId(List<ObjectResource> objectResources, String fileId) {
+        return objectResources
+                .stream().filter(obj -> obj.getId() != null && obj.getId().equals(fileId))
+                .findAny();
+    }
+
+    private Optional<String> getAttribute(Node node, String name) {
+        final Node namedItem = node.getAttributes().getNamedItem(name);
+        if (namedItem == null) { return Optional.empty(); }
+
+        final String nodeValue = namedItem.getNodeValue();
+        return nodeValue == null ? Optional.empty() : Optional.of(nodeValue);
+    }
+
+    private void setAttribute(Document document, Node node, String name, String value) {
+        final NamedNodeMap attributes = node.getAttributes();
+        final Node newAttribute = document.createAttribute(name);
+        newAttribute.setNodeValue(value);
+        attributes.setNamedItem(newAttribute);
+    }
+
+    private Optional<Node> getFirstChildByLocalName(Node parent, String localName) {
+        final NodeList childNodes = parent.getChildNodes();
 
         for (int i = 0; i < childNodes.getLength(); i++) {
             final Node item = childNodes.item(i);
-            if (item.getLocalName() != null && item.getLocalName().equalsIgnoreCase("flocat")) {
-                return item;
+            if (item.getLocalName() != null && item.getLocalName().equalsIgnoreCase(localName)) {
+                return Optional.of(item);
             }
         }
-        return null;
+
+        return Optional.empty();
     }
 }
