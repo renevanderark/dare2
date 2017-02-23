80facabe60cf32a23b1692182e4870332036cdfe
Rene van der Ark <r.van.der.ark@gmail.com>
2017-02-23 12:02:21 +0100

    comments out perf bit.

--- src/main/scripts/node/src/index.js
+++ src/main/scripts/node/src/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import router from "./router";
 import { connectSocket } from "./bootstrap";
+/*
 import Perf from "react-addons-perf";
 
 Perf.start();
@@ -11,6 +12,7 @@ window.setTimeout(() => {
     const measurements = Perf.getLastMeasurements();
     Perf.printInclusive(measurements);
 }, 10000);
+*/
 
 document.addEventListener("DOMContentLoaded", () =>
     connectSocket(() => ReactDOM.render(router, document.getElementById("app"))));
